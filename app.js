fb81b8616ad1a7261a4cdba4c85c4b67b5749014
Daniils Rudijs <daniils.rudijsdp11@gmail.com>
2021-03-16 15:24:28 +0200

    15.lesson

--- app.js
+++ app.js
@@ -31,5 +31,14 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
+});
 
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
 });
